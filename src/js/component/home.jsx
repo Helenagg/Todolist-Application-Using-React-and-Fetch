dda7a546c025f7b1db89a3ae27175ba905bf42c6
Helenagg <hgonzalezg18@gmail.com>
2022-10-31 20:36:43 +0000

    creado PUT

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,10 +8,11 @@ const Home = () => {
 	//const list = ['Hello', 'Hello','Hello']	
 	const [loading, setLoading] = useState(true);
 	const [task, setTask] = useState([]);
+	const [inputValue, setInputValue] = useState("");
 
 	useEffect (() => {
 		//console.log('Hola');
-		fetch('https://assets.breatheco.de/apis/fake/todos/user/testApiFs27', {method: 'GET'})
+		fetch('https://assets.breatheco.de/apis/fake/todos/user/testApiFs27', { method: 'GET' })
 		.then(response => 
 			response.json()
 			).then(response => {
@@ -21,11 +22,38 @@ const Home = () => {
 			}
 	)}, []);
 
+	const collectInput = (event) => {
+		setInputValue(event.target.value);		
+	}
+
+	const addTask = () => {
+		//task.push({"label":inputValue,"done":false})
+		setTask(
+			task.concat({"label":inputValue,"done":false})
+		)
+		fetch('https://assets.breatheco.de/apis/fake/todos/user/testApiFs27', {
+			method: 'PUT',
+			body: JSON.stringify(task.concat({"label":inputValue,"done":false})),
+      		headers: {
+        	"Content-Type": "application/json"
+			}
+		})
+		.then(response =>
+			response.json()
+			.then(response => {
+		
+		}))
+
+	}
+
 	return (
 		<>
 			<div className="text-center pt-5">
 				<h1>Task list</h1>
+				<input placeholder="Add task" onChange={collectInput}></input>
+				<p>{inputValue}</p>
 				{loading ? "Loading..." : <div  className="d-grid gap-2 col-3 p-5 mx-auto">{task.map(element => { return <p className="border border-2 border-light">{element.label}</p>})}
+				<button onClick={addTask}>Add to task</button>
 				</div>}
 			</div>
 		</>
